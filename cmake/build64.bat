8f8746508704515522dfef1d343cbc3aa0a751fd
petasis <petasisg@yahoo.gr>
2016-09-08 11:25:13 +0300

    Updated CMAKE build system

--- cmake/build64.bat
+++ cmake/build64.bat
@@ -1,3 +1,4 @@
+@echo off
 REM ==========================================================================
 REM  Run cmake with a suitable (i.e. installed) generator!
 REM ==========================================================================
@@ -21,16 +22,17 @@ REM   Eclipse CDT4 - MinGW Makefiles  = Generates Eclipse CDT 4.0 project files.
 REM   Eclipse CDT4 - NMake Makefiles  = Generates Eclipse CDT 4.0 project files.
 REM   Eclipse CDT4 - Unix Makefiles   = Generates Eclipse CDT 4.0 project files.
 
-REM mkdir vs-win32
-REM cd vs-win32
-REM cmake -G "Visual Studio 9 2008" -D CPACK_BINARY_NSIS=ON -D CPACK_BINARY_ZIP=ON -D CMAKE_INSTALL_PREFIX=../runtime ..\..
+REM mkdir vs-win64
+REM cd vs-win64
+REM cmake -G "Visual Studio 9 2008 Win64" -D PKG_OS_ARCH=x86_64 -D CMAKE_INSTALL_PREFIX=../runtime ..\..
 REM cd ..
 
-REM mkdir debug-nmake-win32
-REM cd debug-nmake-win32
-REM cmake -G "NMake Makefiles" -D CPACK_BINARY_NSIS=ON -D CPACK_BINARY_ZIP=ON -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=../runtime ..\..
+REM mkdir debug-nmake-win64
+REM cd debug-nmake-win64
+REM cmake -G "NMake Makefiles" -D PKG_OS_ARCH=x86_64 -D CPACK_BINARY_NSIS=ON -D CPACK_BINARY_ZIP=ON -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=../runtime ..\..
 REM cd ..
-mkdir release-nmake-win32
-cd release-nmake-win32
-cmake -G "NMake Makefiles" -D CPACK_BINARY_NSIS=ON -D CPACK_BINARY_ZIP=ON -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=../runtime ..\..
+
+mkdir release-nmake-win64
+cd release-nmake-win64
+cmake -G "NMake Makefiles" -D PKG_OS_ARCH=x86_64 -D CPACK_BINARY_NSIS=ON -D CPACK_BINARY_ZIP=ON -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=../runtime ..\..
 cd ..
